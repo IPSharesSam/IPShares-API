eb99f9316375f14d0c8b2e68048f5ce626a7a95d
mauricebuiten <maubui@gmail.com>
2017-11-23 18:21:07 +0100

    Add scraper script (not working yet)

--- models/property.js
+++ models/property.js
@@ -2,9 +2,14 @@ const mongoose = require('../config/database')
 const { Schema } = mongoose
 
 const propertieschema = new Schema({
+  trademark_name: {type: String },
+  application_number: {type: String },
+  application_language: {type: String },
+  application_date: {type: String },
   userId: { type: Schema.Types.ObjectId, ref: 'users' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
+
 
 module.exports = mongoose.model('properties', propertieschema)
