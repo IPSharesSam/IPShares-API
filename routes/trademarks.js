2d5f3c653eec28acab7d632cdaa45cfa97fbf7f2
mauricebuiten <maubui@gmail.com>
2017-11-28 11:33:53 +0100

    Create fixtures

--- routes/trademarks.js
+++ routes/trademarks.js
@@ -7,56 +7,56 @@ const authenticate = passport.authorize('jwt', { session: false })
 
 const exec = require('sync-exec');
 
-function startScript(input) {
-  const result = exec(`ruby scripts/scrape.rb "${input}"`)
-  
-  return JSON.parse(result.stdout)
-}
-
 // function startScript(input) {
-//   return [
-//     {
-//         "owner_name": input,
-//         "trademark_number": "0987654321",
-//         "trademark_name": "The Cool Company",
-//         "application_date": "09-09-2009",
-//         "registration_date": "10-10-2010",
-//         "status": "registered"
-//     },
-//     {
-//         "owner_name": input,
-//         "trademark_number": "1234567890",
-//         "trademark_name": "TCC",
-//         "application_date": "10-10-2010",
-//         "registration_date": "11-11-2011",
-//         "status": "expired"
-//     },
-//     {
-//         "owner_name": input,
-//         "trademark_number": "5432109876",
-//         "trademark_name": "Company Cool",
-//         "application_date": "11-11-2011",
-//         "registration_date": "12-12-2012",
-//         "status": "refused"
-//     },
-//     {
-//         "owner_name": input,
-//         "trademark_number": "6789012345",
-//         "trademark_name": "Way Cool",
-//         "application_date": "12-12-2012",
-//         "registration_date": "13-13-2013",
-//         "status": "registered"
-//     },
-//     {
-//         "owner_name": input,
-//         "trademark_number": "0987612345",
-//         "trademark_name": "Coolio",
-//         "application_date": "13-13-2013",
-//         "registration_date": "14-14-2014",
-//         "status": "registered"
-//     }
-// ]
+//   const result = exec(`ruby scripts/scrape.rb "${input}"`)
+  
+//   return JSON.parse(result.stdout)
 // }
+
+function startScript(input) {
+  return [
+    {
+        "owner_name": input,
+        "trademark_number": "0987654321",
+        "trademark_name": "The Cool Company",
+        "application_date": "09-09-2009",
+        "registration_date": "10-10-2010",
+        "status": "registered"
+    },
+    {
+        "owner_name": input,
+        "trademark_number": "1234567890",
+        "trademark_name": "TCC",
+        "application_date": "10-10-2010",
+        "registration_date": "11-11-2011",
+        "status": "expired"
+    },
+    {
+        "owner_name": input,
+        "trademark_number": "5432109876",
+        "trademark_name": "Company Cool",
+        "application_date": "11-11-2011",
+        "registration_date": "12-12-2012",
+        "status": "refused"
+    },
+    {
+        "owner_name": input,
+        "trademark_number": "6789012345",
+        "trademark_name": "Way Cool",
+        "application_date": "12-12-2012",
+        "registration_date": "13-13-2013",
+        "status": "registered"
+    },
+    {
+        "owner_name": input,
+        "trademark_number": "0987612345",
+        "trademark_name": "Coolio",
+        "application_date": "13-13-2013",
+        "registration_date": "14-14-2014",
+        "status": "registered"
+    }
+]
+}
 
 router.get('/trademarks', (req, res, next) => {
   Trademark.find()
